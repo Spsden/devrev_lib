f0498df164a6669f5f1211265123c4b04ca80f21
Spsden <pratapsinghsuraj420@gmail.com>
2023-09-23 01:58:24 +0530

    added tailwind support

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Feed from './component/feed/feed';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-blue-600">
+      <Feed/>
     </div>
   );
 }
 
 export default App;
+
+
+ function Test() {
+  return (
+    <h1 className="absolute text-center w-[5000px] -translate-y-2/4 -translate-x-2/4 left-2/4 top-[135px]">
+      Hello world!
+    </h1>
+  )
+}
