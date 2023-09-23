70588ed698cdf864966e9cdf7e6b5e472228a735
Spsden <pratapsinghsuraj420@gmail.com>
2023-09-24 03:58:48 +0530

    navigates to searchpage

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Routes, Route } from "react-router-dom";
 import User from './pages/UserPage/user';
 import { Navigate } from "react-router-dom";
 import CommonLayout from "./component/commonLayout/commonLayout";
+import SearchResults from "./pages/SeachResults/searchResults";
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
       <Route path="/" element={<CommonLayout />}>
       <Route path="home" element={<Home />} />
       <Route path="user" element={<User />} />
+      <Route path='/search/:searchTerm' element={<SearchResults />} />
+     
         <Route path="*" element={<Navigate to="/home" replace />} />
       </Route>
 
--- src/component/searchbar/searchbar.jsx
+++ src/component/searchbar/searchbar.jsx
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
+import { useNavigate,useSearchParams,useLocation } from "react-router-dom";
 
 const SearchBar = () => {
-  const [dropDown, showDropDown] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
 
-  const handleDropDown = () => {
-    showDropDown(!dropDown);
+  const onhandleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm('');
+    }
   };
-
   const DropDown = () => {
     return (
       <select
-        name="cars"
+        name="Filters"
         id="cars"
         className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
       >
-        <option value="volvo">Volvo</option>
-        <option value="saab">Saab</option>
-        <option value="opel">Opel</option>
-        <option value="audi">Audi</option>
+        <option value="title">Title</option>
+        <option value="artist">Artists</option>
+        <option value="writer">Writer</option>
+        <option value="published">published</option>
       </select>
       // <div
       //   id="dropdown"
@@ -68,17 +75,12 @@ const SearchBar = () => {
     <div>
       <form>
         <div className="flex">
-          <label
-            htmlFor="search-dropdown"
-            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-          >
-            Your Email
-          </label>
-          <DropDown/>
+        
         
 
           <div className="relative w-full">
             <input
+            onChange={(x) => {setSearchTerm(x.target.value)}}
               type="search"
               id="search-dropdown"
               className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
@@ -86,6 +88,7 @@ const SearchBar = () => {
               required=""
             />
             <button
+            onClick={onhandleSubmit}
               type="submit"
               className="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
