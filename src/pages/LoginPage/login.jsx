8728e4d7b2da9d4d9d58342a7bc76e4830588cb9
Spsden <pratapsinghsuraj420@gmail.com>
2023-09-25 03:01:30 +0530

    auth workflow signup works

--- src/pages/LoginPage/login.jsx
+++ src/pages/LoginPage/login.jsx
@@ -1,43 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../component/homePage/header";
+import { useSignIn } from "react-auth-kit";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = useSignIn();
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    const userData = {
+      email,
+      password,
+    };
+
+    try {
+      const response = await fetch("https://booksdbdep.onrender.com/api/v1/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+
+      if (response.ok) {
+        console.log("Signup successful");
+
+        const data = await response.json();
+        const username = data.user.name;
+        const token = data.token;
+        console.log(username)
+        signIn({
+          token: token,
+          tokenType: "Bearer",
+          expiresIn: 3600,
+          authState: { email: username },
+        });
+      } else {
+        console.error("Signup failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
   return (
     <div>
-    
-    <div className="bg-gray-50 dark:bg-gray-900">
-  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-    <p
-      className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-    >
-      <img
-        className="w-8 h-8 mr-2"
-        src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-        alt="logo"
-      />
-      Dev Library
-    </p>
-    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-      <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-          Create and account
-        </h1>
-        <form className="space-y-4 md:space-y-6" action="#">
-        <div>
-            <label
-              htmlFor="Name"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              UserName
-            </label>
-            <input
-              type="text"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Enter Your name"
-              required=""
-            />
-          </div>
-          {/* <div>
+      <div className="bg-gray-50 dark:bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <p className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            <img className="w-8 h-8 mr-2" src="/logo512.png" alt="logo" />
+            Learning Hub
+          </p>
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Login
+              </h1>
+              <form className="space-y-4 md:space-y-6" onSubmit={handleSignup}>
+                <div>
+                  <label
+                    htmlFor="Name"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    UserName
+                  </label>
+                  <input
+                    type="text"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter Your name"
+                    required=""
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                {/* <div>
             <label
               htmlFor="email"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -53,47 +88,38 @@ function Login() {
               required=""
             />
           </div> */}
-          <div>
-            <label
-              htmlFor="password"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="••••••••"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required=""
-            />
-          </div>
-        
-          <button
-            type="submit"
-            className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-          >
-            Login
-          </button>
-          <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-            Already have an account?{" "}
-          <a href='/login'>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
 
-            <p
-              className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-            >
-              Login here
-            </p>
-          </a>
-          </p>
-        </form>
+                <button
+                  type="submit"
+              
+                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Login
+                </button>
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400"></p>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-</div>
-
   );
 }
 
