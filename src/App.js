bff41f49744567c4ea607d8d90c0ea1ba8de31d9
Spsden <pratapsinghsuraj420@gmail.com>
2023-09-24 21:54:46 +0530

    redirects to home page

--- src/App.js
+++ src/App.js
@@ -9,19 +9,16 @@ import { Navigate } from "react-router-dom";
 import CommonLayout from "./component/commonLayout/commonLayout";
 import SearchResults from "./pages/SeachResults/searchResults";
 import { useContext } from "react";
-import { AuthContext } from './utiils/context/Auth.context.js';
-
+import { AuthContext } from "./utiils/context/Auth.context.js";
 
 function App() {
-
-
-
   return (
     <Routes>
       <Route path="/" element={<CommonLayout />}>
+        <Route index element={<Home />} />
         <Route path="home" element={<Home />} />
         <Route path="user" element={<User />} />
-        
+
         <Route path="/search/:searchTerm" element={<SearchResults />} />
 
         <Route path="*" element={<Navigate to="/home" replace />} />
