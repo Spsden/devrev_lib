eab96106c0e34995b44a4222038756c4ad3a1901
Spsden <pratapsinghsuraj420@gmail.com>
2023-09-25 02:37:11 +0530

    changes to searchresults

--- src/pages/SeachResults/searchResults.jsx
+++ src/pages/SeachResults/searchResults.jsx
@@ -5,8 +5,32 @@ import Card2 from "../../component/card/card2";
 import { MoonLoader } from "react-spinners";
 import { useParams } from "react-router-dom";
 
+// Book.js
 
-function SearchResults({redirectionTo}) {
+function Book({ title, author, genre, coverImage }) {
+  return (
+    <div className="w-52 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+      <img
+        src={coverImage}
+        alt="Product"
+        className="h-52 w-52 object-cover rounded-t-xl"
+      ></img>
+
+      <div className="px-4 py-2 w-48">
+        <span className="text-gray-400 mr-3 uppercase text-xs">{author}</span>
+        <p className="text-lg font-bold text-black truncate block capitalize">
+          {title}
+        </p>
+        <p className="text-sm text-gray-600 cursor-auto ml-2">10</p>
+        <div className="flex items-center">
+          <div className="ml-auto"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function SearchResults({ redirectionTo }) {
   const { searchTerm } = useParams();
   console.log(searchTerm);
   const listInnerRef = useRef();
@@ -17,7 +41,6 @@ function SearchResults({redirectionTo}) {
   // State variable for the API key
 
   const key = "AIzaSyBsWNaSjQ41Q_AXHNLAaBr7J_o8AsCevKw";
-
 
   useEffect(() => {
     const fetchData = async () => {
@@ -45,11 +68,9 @@ function SearchResults({redirectionTo}) {
   }, [currPage, lastPage, prevPage, bookList]);
 
   useEffect(() => {
-  setCurrPage(0)
-  setBookList([])
-    
-  }, [searchTerm])
-  
+    setCurrPage(0);
+    setBookList([]);
+  }, [searchTerm]);
 
   const onScroll = () => {
     if (listInnerRef.current) {
@@ -74,15 +95,17 @@ function SearchResults({redirectionTo}) {
 
             overflow: "auto",
           }}
-          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 	"
+          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
         >
           {bookList &&
             bookList.length > 0 &&
-            bookList.map((i, index) => (
-              <Card2
+            bookList.map((book, index) => (
+              <Book
                 key={index}
-                title={i.volumeInfo.title}
-                img_src={i.volumeInfo?.imageLinks?.thumbnail}
+                title={book.volumeInfo.title}
+                author={book.volumeInfo?.authors?.join(" ")}
+                genre={book.volumeInfo?.categories?.join(" ")}
+                coverImage={book.volumeInfo?.imageLinks?.thumbnail}
               />
             ))}
         </div>
